e264cd44ad4bd5125df308da7c32b901ec237b6b
Jake Donaldson <Jdonaldson8@mail.greenriver.edu>
2021-04-20 18:14:38 -0400

    created composer and index files

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+//this is my controller for the dating project
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//require autoload file
+require_once('vendor/autoload.php');
+
+//instantiate fat-free
+$f3 = Base::instance();
+
+//define default route
+$f3->route('GET /', function(){
+    //display the home page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//run fat-free
+$f3->run();
