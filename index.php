a9c6ce73b3b6288a4ae8d876858d372a2013be85
Jake Donaldson <Jdonaldson8@mail.greenriver.edu>
2021-05-04 19:00:42 -0400

    moved images outside of views folder

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 //require autoload file
 require_once('vendor/autoload.php');
 
@@ -14,7 +17,26 @@ $f3 = Base::instance();
 $f3->route('GET /', function(){
     //display the home page
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/info.html');
+});
+
+$f3->route('GET|POST /personal', function(){
+    //if the form has been submitted, add the data to session
+    //and send the user to the next order form
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+        $_SESSION['fname'] = $_POST['fname'];
+        $_SESSION['lname'] = $_POST['lname'];
+        $_SESSION['age'] = $_POST['age'];
+        $_SESSION['gender'] = $_POST['gender'];
+        $_SESSION['phone'] = $_POST['phone'];
+        header('location: interests');
+    }
+
+    //Display the personal_info page
+    $view = new Template();
+    echo $view->render('views/personal_info.html');
 });
 
 //run fat-free
